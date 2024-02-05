d75d8ede069f8a60a76c4bf5a502066812a774de
abmnq <fireballthered@gmail.com>
2024-02-05 20:59:27 +0000

    End of V2

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
+const {storage} = require('@google-cloud/storage');
+const csv = require("csv-parser");
+
+
 exports.readObservation = (file, context) => {
-    console.log(`  Event: ${context.eventId}`);
-    console.log(`  Event Type: ${context.eventType}`);
-    console.log(`  Bucket: ${file.bucket}`);
-    console.log(`  File: ${file.name}`);
+    // console.log(`  Event: ${context.eventId}`);
+    // console.log(`  Event Type: ${context.eventType}`);
+    // console.log(`  Bucket: ${file.bucket}`);
+    // console.log(`  File: ${file.name}`);
+
+const gcs = new Storage();
+
+const dataFile = gcs.bucket(file.bucket).file(file.name);
+
+
+dataFile.CreateReadStream()
+
+.on('error', ()=>{
+console.error(error);
+
+
+})
+.pipe(csv())
+.on('data', (row)=>{
+    console.log(row)
+})
+.on('end', ()=>{
+    console.log("End!")
+})
 }
