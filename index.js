c76bd8585382342dcbd757f7f7af00dfa3bc28da
abmnq <fireballthered@gmail.com>
2024-02-12 02:25:57 +0000

    End of v3 - helper function

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const {storage} = require('@google-cloud/storage');
-const csv = require("csv-parser");
-
+const {Storage} = require('@google-cloud/storage');
+const csv = require('csv-parser');
 
 exports.readObservation = (file, context) => {
     // console.log(`  Event: ${context.eventId}`);
@@ -8,25 +7,32 @@ exports.readObservation = (file, context) => {
     // console.log(`  Bucket: ${file.bucket}`);
     // console.log(`  File: ${file.name}`);
 
-const gcs = new Storage();
+    const gcs = new Storage();
 
-const dataFile = gcs.bucket(file.bucket).file(file.name);
+    const dataFile = gcs.bucket(file.bucket).file(file.name);
 
-
-dataFile.CreateReadStream()
-.on('error', ()=>{
-console.error(error);
-})
-.pipe(csv())
-
-
-.on('data', (row)=>{
-    console.log(row)
-})
-
-
-.on('end', ()=>{
-    console.log("End!")
-})
+    dataFile.createReadStream()
+    .on('error', () => {
+        // Handle an error
+        console.error(error);
+    })
+    .pipe(csv())
+    .on('data', (row) => {
+        // Log row data
+        // console.log(row);
+        printDict(row);
+    })
+    .on('end', () => {
+        // Handle end of CSV
+        console.log('End!');
+    })
 }
 
+// HELPER FUNCTIONS
+
+function printDict(row) {
+    for (let key in row) {
+        console.log(key + ' : ' + row[key]);
+        console.log(`${key} : ${row[key]}`);
+    }
+}
