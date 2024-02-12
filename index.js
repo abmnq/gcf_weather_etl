78f4608492f8d915cd448abcb3b3e9b98c5f7d70
abmnq <70235511+abmnq@users.noreply.github.com>
2024-02-12 06:12:58 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,38 +1,86 @@
+// const {BigQuery} = require('@google-cloud/bigquery');
+// const bq = new BigQuery();
+// const datasetId = "weather_etl"
+// const tableId = "demo"
+
+// const demoCode =()=>{
+
+// fakeObject = {};
+// fakeObject.first_name = "Ahmed";
+// fakeObject.last_name = "Mohammed";
+// fakeObject.email ="ahmoha@iu.edu";
+// fakeObject.age = 28;
+// writeToBq(fakeObject);
+// }
+
+// demoCode();
+
+// //create a help function that writes to BQ
+
+// async function writeToBq(obj){
+// //BQ expects an array of objects, but this function only receives 1
+
+// var rows=[];
+// rows.push(obj);
+// await bq
+// .dataset(datasetId)
+// .table(tableId)
+// .insert(rows)
+// .then (()=> {
+//     rows.forEach ((row)=>{console.log(`inserted: ${row}`)})
+// })
+// .catch ((err)=>{console.error(`ERROR: ${err}`)})
+// }
+
+
+
+const {BigQuery} = require('@google-cloud/bigquery');
 const {Storage} = require('@google-cloud/storage');
 const csv = require('csv-parser');
 
-exports.readObservation = (file, context) => {
-    // console.log(`  Event: ${context.eventId}`);
-    // console.log(`  Event Type: ${context.eventType}`);
-    // console.log(`  Bucket: ${file.bucket}`);
-    // console.log(`  File: ${file.name}`);
+const gcs = new Storage();
+const bq = new BigQuery();
 
-    const gcs = new Storage();
+const datasetId = "weather_etl";
+const tableId = "weatherindy";
 
+exports.transferFive = (file, context) => {
     const dataFile = gcs.bucket(file.bucket).file(file.name);
-
+    let rows = []; 
     dataFile.createReadStream()
-    .on('error', () => {
-        // Handle an error
-        console.error(error);
-    })
     .pipe(csv())
     .on('data', (row) => {
-        // Log row data
-        // console.log(row);
-        printDict(row);
-    })
-    .on('end', () => {
-        // Handle end of CSV
-        console.log('End!');
-    })
-}
+        const transformedRow = transformRow(row, file.name);
+        if (transformedRow) { 
+            rows.push(transformedRow); }
+        })
 
-// HELPER FUNCTIONS
-
-function printDict(row) {
-    for (let key in row) {
-        console.log(key + ' : ' + row[key]);
-        console.log(`${key} : ${row[key]}`);
-    }
-}
+        .on('end', async () => {
+            if (rows.length > 0) {
+                await writeToBq(rows).catch(err => console.error('ERROR:', err));
+            }
+            console.log('COMPLETED:');
+        })};
+        function transformRow(row, fileName) {
+        
+            const stationIdPattern = /\d{6}-\d{5}/;
+            const stationIdMatch = fileName.match(stationIdPattern);
+            const stationId = stationIdMatch ? stationIdMatch[0] : null;
+            row.station = stationId;
+            const floating = ['airtemp', 'dewpoint', 'pressure', 'windspeed', 'precip1hour', 'precip6hour'];
+        
+            Object.keys(row).forEach(key => {
+                const value = parseInt(row[key],10)
+                
+                if (value === -9999) {
+                    row[key] = null; 
+                } else if (floating.includes(key)) {
+                    row[key] = parseFloat(row[key]) / 10;
+                } 
+            });
+        
+            return row;
+        }
+        async function writeToBq(rows) {
+            await bq.dataset(datasetId).table(tableId).insert(rows);
+        }
